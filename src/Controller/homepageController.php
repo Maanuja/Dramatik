9e78aa0c1c640aa2c334cf32533e27b4123ca8fd
Maanuja Sutharsan <maanuja.sutharsan@etu.univ-paris1.fr>
2022-01-11 14:36:56 +0100

    initialize project

---
+++ src/Controller/homepageController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class homepageController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function home(): Response
+    {
+        return new Response('HomePage');
+    }
+
+    /**
+     * @Route("/contact")
+     */
+    public function contact(): Response
+    {
+        return new Response( 'PageContact');
+    }
+
+    /**
+    * @Route("hello/{firstname<[A-Z a-z]+>}/{lastname<[A-Z a-z]+>}")
+    */
+    public function hello(string $lastname, string $firstname): Response
+    {
+        return new Response(sprintf('coucou %s %s', $firstname, $lastname));
+    }
+}

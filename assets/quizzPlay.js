34527a2343b783ff51bfd17153d32d4d0f7bba73
Mithunah Mathikaran <mithunah1119@gmail.com>
2022-03-21 00:25:03 +0100

    feat: quizz creation

--- assets/quizzPlay.js
+++ assets/quizzPlay.js
@@ -1,1 +1,18 @@
 require('./styles/quizzPlay.css');
+
+const btn = document.getElementById("nxtqst").addEventListener("click", displayNext(document.getElementById("nextqst").value));
+
+
+function displayNext(number) {
+    var tab = document.getElementById("question"+number);
+    var tabN = document.getElementById("question"+number+1);
+    tab.style.display=" none !important";
+    if (tab.style.display !== " none !important") {
+        tab.style.display = " none !important";
+    } else {
+        tab.style.display = "flex !important";
+    }
+    tabN.style.display="flex !important";
+    //alert(number +"H eight and Width must not be under 1080px for height and 1920px for width.");
+    return true;
+    }
